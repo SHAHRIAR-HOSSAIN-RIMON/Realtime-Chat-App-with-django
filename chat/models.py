ff8aa24c8154c4a79526914b36de27dd8607d0a3
SHAHRIAR-HOSSAIN-RIMON <mdshrhussain@gmail.com>
2025-06-13 03:26:11 +0600

    project is completed lot of changes in models

--- chat/models.py
+++ chat/models.py
@@ -4,8 +4,9 @@ from datetime import datetime
 # Create your models here.
 class Room(models.Model):
     name = models.CharField(max_length=1000)
+
 class Message(models.Model):
-    vale=models.CharField(max_length=10000000)
-    date =models.DateTimeField(default=datetime.now, blank=True)
-    user=models.CharField(max_length=10000000)
-    room=models.CharField(max_length=10000000)
+    value = models.CharField(max_length=1000, default="")
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    user = models.CharField(max_length=255)
+    room = models.CharField(max_length=255)
--- chat/views.py
+++ chat/views.py
@@ -1,21 +1,21 @@
 from django.shortcuts import render, redirect
 from chat.models import Room,Message
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 # Create your views here.
 def home(request):
-    return render(request,'home.html') 
-def room(request,room):
+    return render(request, 'home.html')
+
+def room(request, room):
     username = request.GET.get('username')
     room_details = Room.objects.get(name=room)
-    return render(request,'home.html',{
+    return render(request, 'room.html', {
         'username': username,
         'room': room,
         'room_details': room_details
-        
     })
 def checkview(request):
     room = request.POST['room_name']
-    username = request.POST['user_name']
+    username = request.POST['username']
 
     if Room.objects.filter(name=room).exists():
         return redirect('/'+room+'/?username='+username)
@@ -31,4 +31,7 @@ def send(request):
     new_message = Message.objects.create(value=message,user=username,room=room_id)
     new_message.save()
     return HttpResponse('Message sent successfully!')
-    
+def getMessages(request,room):
+    room_details = Room.objects.get(name=room)
+    messages = Message.objects.filter(room =room_details.id)
+    return JsonResponse({"messages":list(messages.values())})
