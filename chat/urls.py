4b59e2bff4df285beeb7d178b65382d9e15020a0
SHAHRIAR-HOSSAIN-RIMON <mdshrhussain@gmail.com>
2025-06-12 23:46:34 +0600

    features

--- chat/urls.py
+++ chat/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .import views
+from . import views
 
-urlpatterns=[
+urlpatterns = [
     path('', views.home, name='home'),
+    path('checkview/', views.checkview, name='checkview'),
+    path('<str:room>/', views.room, name='room'),
+    path('send/', views.send, name='send'),
 ]
